10498433bfd384cdd1ee5fe8301f9d3ae92104a7
Waseem963 <39030201+Waseem963@users.noreply.github.com>
2024-12-03 06:34:37 +0100

    Ensembles_next_week.sql

--- Ensembles_next_week.sql
+++ Ensembles_next_week.sql
@@ -11,7 +11,6 @@ select
 from ensemble e
 left join lesson l on l.lessonid = e.lessonid
 left join (select b.lessonid, COUNT(*) from booking b group by b.lessonid) b on b.lessonid = e.lessonid
-left join price_scheme ps on ps.pricingid = l.pricingid
    WHERE EXTRACT('week' FROM l.scheduledTime) = EXTRACT('week' FROM CURRENT_DATE) + 1
   AND EXTRACT('year' FROM l.scheduledTime) = EXTRACT('year' FROM CURRENT_DATE)
 
